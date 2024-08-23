c36634c454ed9880a3d5a09065baa6d2b408a4ec
Viktor_MN <mikhaylovvn1101@gmail.com>
2024-08-23 22:31:04 +0500

    устранение ошибок в TestRunner и poam

--- src/test/java/org/ibstraining/runner/TestRunner.java
+++ src/test/java/org/ibstraining/runner/TestRunner.java
@@ -1,19 +1,17 @@
 package org.ibstraining.runner;
 
 
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.ConfigurationParameters;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 
 import static io.cucumber.core.options.Constants.*;
 
 @Suite
 @IncludeEngines("cucumber")
+@SelectClasspathResource("src/test/java/org/ibstraining/steps")
 @ConfigurationParameters({
         @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@all"),
         @ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "src/test/resources/features"),
-        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "src/test/java/org/ibstraining/steps"),
+        @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org/ibstraining/steps"),
         @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty")
 })
 public class TestRunner {
